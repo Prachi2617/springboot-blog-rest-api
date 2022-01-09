5cc8157a33e8e498101438e1040c7c87c4026211
Prachi2617 <prachidd1234@gmail.com>
2022-01-09 20:31:43 +0530

    Initial commit

---
+++ src/main/java/com/springboot/blog/entity/post.java
@@ -0,0 +1,33 @@
+package com.springboot.blog.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(
+        name = "posts", uniqueConstraints = {@UniqueConstraint(columnNames = {"title"})}
+)
+public class post
+{
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.IDENTITY
+    )
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+}

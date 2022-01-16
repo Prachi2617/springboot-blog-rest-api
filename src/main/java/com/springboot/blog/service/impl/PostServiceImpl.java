d4843a452214c958a9bf7bf5f2567b100b980340
Prachi2617 <prachidd1234@gmail.com>
2022-01-16 23:21:01 +0530

    modelmapper library used

--- src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
+++ src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
@@ -7,6 +7,7 @@ import com.springboot.blog.payload.PostResponse;
 import com.springboot.blog.repository.PostRepository;
 import com.springboot.blog.service.PostService;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -21,10 +22,13 @@ import java.util.stream.Collectors;
 public class PostServiceImpl implements PostService
 {
     private PostRepository postRepository;
+    private ModelMapper mapper;
 
     //omit @Autowired annotation as it has only one constructor
-    public PostServiceImpl(PostRepository postRepository) {
+    public PostServiceImpl(PostRepository postRepository, ModelMapper mapper)
+    {
         this.postRepository = postRepository;
+        this.mapper=mapper;
     }
 
     @Override
@@ -103,11 +107,12 @@ public class PostServiceImpl implements PostService
     //convert entity to dto
     private PostDto mapToDto(Post post)
     {
-        PostDto postDto= new PostDto();
+        PostDto postDto=mapper.map(post,PostDto.class);
+        /*PostDto postDto= new PostDto();
         postDto.setId(post.getId());
         postDto.setTitle(post.getTitle());
         postDto.setDescription(post.getDescription());
-        postDto.setContent(post.getContent());
+        postDto.setContent(post.getContent());*/
         return postDto;
     }
 
@@ -115,10 +120,11 @@ public class PostServiceImpl implements PostService
 
     private Post mapToEntity(PostDto postDto)
     {
-        Post post=new Post();
+        Post post=mapper.map(postDto,Post.class);
+       /* Post post=new Post();
         post.setTitle(postDto.getTitle());
         post.setDescription(postDto.getDescription());
-        post.setContent(postDto.getContent());
+        post.setContent(postDto.getContent());*/
         return post;
     }
 }

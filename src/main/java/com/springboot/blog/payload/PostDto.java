d2ee2abd55b7ea811d221b1cb7f16c658014aef9
Prachi2617 <prachidd1234@gmail.com>
2022-04-02 13:10:45 +0530

    new dependency added

--- src/main/java/com/springboot/blog/payload/PostDto.java
+++ src/main/java/com/springboot/blog/payload/PostDto.java
@@ -8,6 +8,12 @@ import java.util.Set;
 public class PostDto
 {
     private Long id;
+    // title should not be null  or empty
+    // title should have at least 2 characters
+    //Santitycheck -testing
+    //1- notnull, notempty, size
+    //2- No. of comments( integer) cannot be null, and in range
+    //
     private String title;
     private String description;
     private String Content;
